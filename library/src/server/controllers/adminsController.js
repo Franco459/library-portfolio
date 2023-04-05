88aabd39f9684bfec2ad633994615fc059179fb8
Franco459 <francopistonecarp11@gmail.com>
2023-04-05 19:43:09 -0300

    update e insert agregados

--- library/src/server/controllers/adminsController.js
+++ library/src/server/controllers/adminsController.js
@@ -41,7 +41,56 @@ const removeAdmin = async (req, res = response) => {
     })
 }
 
+const updateAdmin = async (req, res = response) => {
+    let adminID = req.params.id;
+    let adminName = req.body.name;
+    let adminSurname = req.body.surname;
+    let adminUsername = req.body.username;
+    let adminTelephone = req.body.telephone;
+    let adminPassword = req.body.password;
+    let adminEmail = req.body.email;
+
+    let sqlQuery = `UPDATE administradores SET name = ${adminName}, surname = ${adminSurname}, username = ${adminUsername}, telephone = ${adminTelephone}, email = ${adminEmail} WHERE id = ${adminID}`;
+
+    dbConnection.query(sqlQuery,(err, results) =>{
+        if(err) console.log(err);
+        if(results.affectedRows == 1){
+            res.status(200).send({
+                message: 'Administrador actualizado.',
+                data: results
+            })
+        }
+        else{
+            res.status(404).send({
+                message:'Administrador no encontrado'
+            })
+        }
+    });
+}
+const createAdmin = (req, res = response) =>{
+    
+    let adminName = req.body.name;
+    let adminSurname = req.body.surname;
+    let adminUsername = req.body.username;
+    let adminTelephone = req.body.telephone;
+    let adminPassword = req.body.password;
+    let adminEmail = req.body.email;
+    
+    let sqlQuery = `INSERT INTO administradores (name, surname, username, telephone, password, email)
+                    values ('${adminName}', '${adminSurname}', '${adminUsername}', '${adminTelephone}', '${adminPassword}', '${adminEmail}')`;
+
+    dbConnection.query(sqlQuery, (err, results) => {
+        if(err) console.log(err);
+        res.status(200).send({
+            message: "User created",
+            data: results
+        })
+    })
+
+}
 module.exports = {
     getAdmins,
-    removeAdmin
+    removeAdmin,
+    updateAdmin, 
+    createAdmin
 }
